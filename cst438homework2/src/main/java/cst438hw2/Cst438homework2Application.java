139a7b528c394266832005d0e07e5a770044a984
cesarsanchez <cesarsanchez@DESKTOP-JQV67KS.home>
2021-05-11 19:33:29 -0700

    initial commit without the Java files

---
+++ cst438homework2/src/main/java/cst438hw2/Cst438homework2Application.java
@@ -0,0 +1,13 @@
+package cst438hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst438homework2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst438homework2Application.class, args);
+	}
+
+}
